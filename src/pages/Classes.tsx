b16347332327f1f4b7411437196f1f9c991142d9
Lucas <lugablima@gmail.com>
2024-01-11 20:29:28 -0300

    feat: adds classes page with the features of getting all, enabling, disabling and deleting classes

---
+++ src/pages/Classes.tsx
@@ -0,0 +1,115 @@
+import styled from "styled-components";
+
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import PageContainer from "../layouts/PageContainer";
+import Card from "../components/Card";
+import Button from "../components/Button";
+import { IUserContext, useUserContext } from "../contexts/UserContext";
+import checkUserAccess from "../hooks/useCheckUserAccess";
+import * as classService from "../services/classService";
+
+export default function Classes() {
+  const [classes, setClasses] = useState<classService.IClass[]>([]);
+  const [update, setUpdate] = useState<boolean>(false);
+  const { user } = useUserContext() as IUserContext;
+  const navigate = useNavigate();
+  const params = useParams<{ moduleId: string }>();
+  const {
+    state: { moduleName },
+  } = useLocation();
+
+  function updateClasses() {
+    setUpdate(!update);
+  }
+
+  useEffect(() => {
+    checkUserAccess(user, navigate);
+
+    async function fetchData(token: string) {
+      try {
+        if (!params.moduleId) return;
+
+        const res = await classService.getAll(token, params.moduleId);
+
+        setClasses(res);
+      } catch (error) {
+        alert(error);
+      }
+    }
+
+    if (user?.token) {
+      fetchData(user.token);
+    }
+  }, [update]);
+
+  function renderClasses() {
+    return classes.map((c) => {
+      if (!user?.isAdmin && !c.isEnabled) return null;
+
+      return (
+        <Card
+          key={c.id}
+          id={c.id}
+          moduleName={c.name}
+          imageUrl={c.imageUrl}
+          isEnabled={c.isEnabled}
+          onClick={() => navigate(`/classes/${c.id}/video`)}
+          updateEntity={() => updateClasses()}
+        />
+      );
+    });
+  }
+
+  return (
+    <PageContainer $display="flex" $justContent="center">
+      <Content>
+        <Header>
+          <Button
+            $w={5.75}
+            $h={2.75}
+            $bgColor="#009FE0"
+            $textColor="#F6F5F4"
+            text="Voltar"
+            $margin="0 0 3rem 0"
+            onClick={() => navigate(-1)}
+          />
+          {user?.isAdmin && (
+            <Button
+              $w={7.625}
+              $h={2.75}
+              $bgColor="#009FE0"
+              $textColor="#F6F5F4"
+              text="Nova aula"
+              $margin="0 0 3rem 0"
+              onClick={() => navigate("/new-class")}
+            />
+          )}
+        </Header>
+        <h5>Aulas • {moduleName}</h5>
+        {classes.length > 0 && renderClasses()}
+      </Content>
+    </PageContainer>
+  );
+}
+
+const Content = styled.div`
+  width: 64.31vw;
+  max-width: 57.875rem;
+  padding-top: 3rem;
+
+  & > h5 {
+    color: var(--black);
+    text-align: center;
+    font-family: Montserrat;
+    font-size: 3rem;
+    font-style: normal;
+    font-weight: 700;
+    margin-bottom: 3rem;
+  }
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
---
+++ src/services/classService.ts
@@ -0,0 +1,91 @@
+/* eslint-disable no-throw-literal */
+import axios, { AxiosResponse } from "axios";
+
+import api from "./api";
+
+export interface IClass {
+  id: string;
+  name: string;
+  imageUrl: string;
+  isEnabled: boolean;
+  moduleId: string;
+  dueDate: Date;
+}
+
+export async function getAll(token: string, moduleId: string) {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response: AxiosResponse<IClass[]> = await api.get(
+      `/classes/${moduleId}`,
+      config,
+    );
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data;
+    } else {
+      throw "Algum erro ocorreu, por favor, recarregue a página!";
+    }
+  }
+}
+
+export async function create() {
+  //
+}
+
+export async function enableOrDisableOne(
+  token: string,
+  classId: string,
+  enableOrDisable: "enable" | "disable",
+) {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response: AxiosResponse<string> = await api.put(
+      `/classes/${classId}/${enableOrDisable}`,
+      null,
+      config,
+    );
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data;
+    } else {
+      throw "Algum erro ocorreu, por favor, recarregue a página!";
+    }
+  }
+}
+
+export async function deleteOne(token: string, classId: string) {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response: AxiosResponse<string> = await api.delete(
+      `/classes/${classId}`,
+      config,
+    );
+
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw error.response?.data;
+    } else {
+      throw "Algum erro ocorreu, por favor, recarregue a página!";
+    }
+  }
+}
